eeba7e61a9749811461ab0384a2936a9c1a44495
Gasol Wu <gasol@pixnet.tw>
2011-08-17 11:43:12 +0800

    bash-completion.sh

---
+++ _bash/rc/bash-completion.sh
@@ -0,0 +1,16 @@
+# Check for interactive bash and that we haven't already been sourced.
+[ -z "$BASH_VERSION" -o -z "$PS1" -o -n "$BASH_COMPLETION" ] && return
+
+# Check for recent enough version of bash.
+bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
+if [ $bmajor -gt 3 ] || [ $bmajor -eq 3 -a $bminor -ge 2 ]; then
+	if [ -r /etc/bash_completion ]; then
+		# Source completion code.
+		. /etc/bash_completion
+	fi
+	if [ -r /usr/local/etc/bash_completion ]; then
+		# Source completion code.
+		. /usr/local/etc/bash_completion
+	fi
+fi
+unset bash bmajor bminor
