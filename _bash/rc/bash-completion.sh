06e04a33edaa16b7e29fefaf71424d15bf586f0f
Gasol Wu <gasolwu@kkbox.com>
2012-08-09 16:58:01 +0800

    fix bash_completion

--- _bash/rc/bash-completion.sh
+++ _bash/rc/bash-completion.sh
@@ -7,8 +7,10 @@ if [ $bmajor -gt 3 ] || [ $bmajor -eq 3 -a $bminor -ge 2 ]; then
 	if [ -r /etc/bash_completion ]; then
 		# Source completion code.
 		. /etc/bash_completion
-	elif [ -x /usr/local/bin/brew -a -f `brew --prefix`/etc/bash_completion ]; then
-		. `brew --prefix`/etc/bash_completion
+	elif [ -x /usr/local/bin/brew ]; then
+		if [ -f `brew --prefix`/etc/bash_completion ]; then
+			. `brew --prefix`/etc/bash_completion
+		fi
 	elif [ -r /usr/local/etc/bash_completion ]; then
 		# Source completion code.
 		. /usr/local/etc/bash_completion
