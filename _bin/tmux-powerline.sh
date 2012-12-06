768d4bd6b22bb81516b0b97b44706fd620764a74
Gasol Wu <gasol.wu@gmail.com>
2012-12-06 17:21:48 +0800

    fix tmux-powerline.sh on MacOS X

--- _bin/tmux-powerline.sh
+++ _bin/tmux-powerline.sh
@@ -3,7 +3,14 @@
 TMUX=$(which tmux)
 TMUX_CONFIG=$($TMUX info | grep configuration | grep -oE '/.*$')
 if [ -L $TMUX_CONFIG ]; then
-	TMUX_CONFIG_DIR=$(dirname $(readlink -f $TMUX_CONFIG))
-	$TMUX set-option -g status-left "#($TMUX_CONFIG_DIR/tmux/status-left.sh)"
-	$TMUX set-option -g status-right "#($TMUX_CONFIG_DIR/tmux/status-right.sh)"
+	TMUX_CONFIG=$(readlink $TMUX_CONFIG)
 fi
+
+if [ "Darwin" = `uname` ]; then
+	TMUX_CONFIG_DIR=$HOME/$(dirname $TMUX_CONFIG)
+else
+	TMUX_CONFIG_DIR=$(dirname $TMUX_CONFIG)
+fi
+
+$TMUX set-option -g status-left "#($TMUX_CONFIG_DIR/tmux/status-left.sh)"
+$TMUX set-option -g status-right "#($TMUX_CONFIG_DIR/tmux/status-right.sh)"
