c1c152f491ad8ad989dbab2ab422dccc51fed929
Gasol Wu <gasol.wu@gmail.com>
2014-01-31 15:31:28 +0800

    User NEW powerline totally rewritten in Python

--- _bin/tmux-powerline.sh
+++ _bin/tmux-powerline.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
 TMUX=$(which tmux)
-TMUX_CONFIG=$($TMUX info | grep configuration | grep -oE '/.*$')
-if [ -L $TMUX_CONFIG ]; then
-	TMUX_CONFIG=$(python -c 'import os,sys; print os.path.realpath(sys.argv[1]);' $TMUX_CONFIG)
-	TMUX_CONFIG_DIR=$(dirname $TMUX_CONFIG)
-fi
-
-$TMUX set-option -g status-left "#($TMUX_CONFIG_DIR/tmux/powerline.sh left)"
-$TMUX set-option -g status-right "#($TMUX_CONFIG_DIR/tmux/powerline.sh right)"
+SITE_DIR=$(python -m site --user-site)
+$TMUX source $SITE_DIR/powerline/bindings/tmux/powerline.conf
