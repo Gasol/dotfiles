8a7e5a9cae0c09e7ccaf058ad2528a4f500bb8c0
Gasol Wu <gasol.wu@gmail.com>
2023-02-13 19:00:45 +0800

    Update ftplugin

---
+++ config/dot-config/nvim/ftplugin/gitrebase.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "S", "<cmd>Cycle<cr>", { silent = true, buffer = true })
---
+++ config/dot-config/nvim/ftplugin/python.lua
@@ -0,0 +1,8 @@
+local opt = vim.opt_local
+opt.softtabstop = 4
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.textwidth = 120
+opt.smarttab = true
+opt.expandtab = true
+opt.commentstring = "#%s"
