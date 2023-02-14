4272291b91e99232563349496206d1dfa4ba384a
Gasol Wu <gasol.wu@gmail.com>
2023-02-14 08:02:25 +0800

    Rename opt to set to be more semantically

--- config/dot-config/nvim/lua/config/options.lua
+++ config/dot-config/nvim/lua/config/options.lua
@@ -1,21 +1,21 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-local opt = vim.opt
-opt.guifont = "Menlo-Regular:h14"
-opt.number = true
-opt.relativenumber = true
-opt.shiftwidth = 4
-opt.termguicolors = true
-opt.hlsearch = true
-opt.background = "dark"
-opt.suffixes = ".bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc"
-opt.updatetime = 300
-opt.scrolloff = 4
-opt.shortmess:append("c")
-opt.pumblend = 15
+local set = vim.opt
+set.guifont = "Menlo-Regular:h14"
+set.number = true
+set.relativenumber = true
+set.shiftwidth = 4
+set.termguicolors = true
+set.hlsearch = true
+set.background = "dark"
+set.suffixes = ".bak,~,.swp,.o,.info,.aux,.log,.dvi,.bbl,.blg,.brf,.cb,.ind,.idx,.ilg,.inx,.out,.toc"
+set.updatetime = 300
+set.scrolloff = 4
+set.shortmess:append("c")
+set.pumblend = 15
 vim.cmd("hi PmenuSel blend=0")
 if vim.fn.executable("rg") == 1 then
-  opt.grepprg = "rg --vimgrep --no-heading --hidden --glob '!.git'"
-  opt.grepformat = "%f:%l:%c:%m"
+  set.grepprg = "rg --vimgrep --no-heading --hidden --glob '!.git'"
+  set.grepformat = "%f:%l:%c:%m"
 end
