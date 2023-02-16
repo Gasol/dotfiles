05c36a47c2c0f6290f47e66d35695347b889edb0
Gasol Wu <gasol.wu@gmail.com>
2023-02-16 23:04:51 +0800

    Install nvim-window-picker

--- config/dot-config/nvim/lua/plugins/general.lua
+++ config/dot-config/nvim/lua/plugins/general.lua
@@ -204,4 +204,16 @@ return {
       })
     end,
   },
+  {
+    "s1n7ax/nvim-window-picker",
+    keys = { "<leader>w", "<leader>e", "<leader>E" },
+    config = function()
+      local picker = require("window-picker")
+      picker.setup()
+      vim.keymap.set("n", "<leader>w", function()
+        local picked_window_id = picker.pick_window() or vim.api.nvim_get_current_win()
+        vim.api.nvim_set_current_win(picked_window_id)
+      end, { desc = "Pick a window" })
+    end,
+  },
 }
