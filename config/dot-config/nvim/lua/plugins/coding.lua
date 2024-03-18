abe1b6d311dfcf0ea2989d4adb373b855d52f9cf
Gasol Wu <gasol.wu@gmail.com>
2024-03-18 13:06:35 +0800

    Update tagalong.vim mappings

--- config/dot-config/nvim/lua/plugins/coding.lua
+++ config/dot-config/nvim/lua/plugins/coding.lua
@@ -25,6 +25,9 @@ return {
   },
   {
     "AndrewRadev/tagalong.vim",
+    config = function()
+      vim.g["tagalong_mappings"] = { "c", "C", "i", "a" }
+    end,
   },
   {
     "akinsho/toggleterm.nvim",
