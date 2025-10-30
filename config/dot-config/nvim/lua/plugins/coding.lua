1567dbe5229ec22f5d2b309aaa4c1b83c33deecf
Gasol Wu <gasol.wu@gmail.com>
2025-10-30 09:56:45 +0800

    Add coding plugins and update dependencies

---
+++ config/dot-config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "vim-test/vim-test",
+    config = function()
+      vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>")
+      vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>")
+      vim.keymap.set("n", "<leader>ts", "<cmd>TestSuite<cr>")
+      vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>")
+      vim.keymap.set("n", "<leader>tg", "<cmd>TestVisit<cr>")
+    end,
+  },
+  {
+    "tpope/vim-scriptease",
+  },
+  {
+    "osyo-manga/vim-jplus",
+    lazy = true,
+    keys = "J",
+    config = function()
+      vim.keymap.set("n", "J", "<Plug>(jplus)")
+    end,
+  },
+  {
+    "yasuhiroki/github-actions-yaml.vim",
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    opts = {
+      open_mapping = [[<F24>]], -- Meta + F12
+    },
+  },
+  {
+    "tpope/vim-rails",
+    ft = "ruby",
+    config = function()
+      vim.keymap.set("n", "<leader>r", "<cmd>Rails<cr>")
+      vim.keymap.set("n", "<leader>R", ":Rails<space>")
+    end,
+  },
+  {
+    "tpope/vim-projectionist",
+    config = function()
+      vim.g.projectionist_heuristics = {
+        ["tests/&phpunit.xml|phpunit.xml.dist"] = {
+          ["src/*.php"] = {
+            alternate = "tests/{}Test.php",
+            type = "source",
+          },
+          ["tests/*Test.php"] = {
+            alternate = "src/{}.php",
+            type = "test",
+          },
+        },
+        ["test/factories"] = {
+          ["test/factories/*.rb"] = {
+            type = "factory",
+            alternate = "app/models/{}.rb",
+          },
+        },
+      }
+    end,
+  },
+}
